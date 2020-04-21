b4f233d93163e9c16bc9bbccfaa3b29f7cfb83f8
Felipe Santos <felipe.santos@argosmonitoramento.com.br>
2020-04-21 11:21:11 -0300

    Primeiro recurso get

--- app.py
+++ app.py
@@ -4,9 +4,38 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel',
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Rio de Janeiro'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'estrelas': 4.5,
+        'diaria': 380.90,
+        'cidade': 'Santa Catarina'
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel',
+        'estrelas': 3.9,
+        'diaria': 320.24,
+        'cidade': 'Rio de Janeiro'
+    }
+]
+
+
 class Hoteis(Resource):
     def get(self):
-        return {'hotelis': 'meus hoteis'}
+        return {'hotelis': hoteis}
+
+
+api.add_resource(Hoteis, '/hoteis')
+
 
 @app.route('/')
 def hello_world():
@@ -14,4 +43,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
