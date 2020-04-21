6d335ee1e233aa085343dbeea461e1361fa09381
Felipe Santos <felipe.santos@argosmonitoramento.com.br>
2020-04-21 11:26:03 -0300

    Refatorando

--- app.py
+++ app.py
@@ -1,38 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+from resources.hotel import Hoteis
+
 app = Flask(__name__)
 api = Api(app)
-
-hoteis = [
-    {
-        'hotel_id': 'alpha',
-        'nome': 'Alpha Hotel',
-        'estrelas': 4.3,
-        'diaria': 420.34,
-        'cidade': 'Rio de Janeiro'
-    },
-    {
-        'hotel_id': 'bravo',
-        'nome': 'Bravo Hotel',
-        'estrelas': 4.5,
-        'diaria': 380.90,
-        'cidade': 'Santa Catarina'
-    },
-    {
-        'hotel_id': 'charlie',
-        'nome': 'Charlie Hotel',
-        'estrelas': 3.9,
-        'diaria': 320.24,
-        'cidade': 'Rio de Janeiro'
-    }
-]
-
-
-class Hoteis(Resource):
-    def get(self):
-        return {'hotelis': hoteis}
-
 
 api.add_resource(Hoteis, '/hoteis')
 
